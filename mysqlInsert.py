c2f5b8b015fb89adb90f3dd08eac110dc698f07d
Jordan Baker <j0bak3r@gmail.com>
2013-12-07 11:44:50 -0800

    seperated insertion function

---
+++ mysqlInsert.py
@@ -0,0 +1,15 @@
+import MySQLdb as mdb
+import conf
+def insert_data(data):
+    try:
+        # Replace the parameters here for using a different mysql server
+        conn = mdb.connect(host=conf.server, user=conf.writer_username, passwd=conf.writer_password, db=conf.db)
+        cur = conn.cursor()
+        for row in data:
+            #Insert each row of the array into the database
+            cur.execute("INSERT INTO gef (source, date, type, cost, units_total, total_cost) VALUES (%s, %s, %s, %s, %s, %s)", (row['source'], row['date'], row['type'], row['cost'], row['units_total'], row['total_cost']))
+        conn.commit()
+        conn.close()
+    except mdb.Error, e:
+        #Print any mysql errors.
+        print "Error %d: %s" % (e.args[0],e.args[1])
